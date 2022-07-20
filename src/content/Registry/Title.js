f9a30bb752bbac5c2dd0318b7d42248825628ad6
acuedd <acuedd@gmail.com>
2022-07-19 22:37:09 -0600

    Updating info about registry

--- src/content/Registry/Title.js
+++ src/content/Registry/Title.js
@@ -8,7 +8,8 @@ export default {
         SubHeading: () => (
             <>
 
-                <p><b>The gift is optional, attend the wedding is mandatory</b></p>
+                <p><b>The gift is optional</b></p>
+                <p><b>Attend the wedding is mandatory</b></p>
                 <p>But if you want to give us a some gift, you can contribute in cash in the pillar-box in the wedding day or
                     enter to this link to check our wish list
                 </p><br />
@@ -21,9 +22,10 @@ export default {
         SubHeading: () => (
             <>
 
-                <p><b>El regalo es opcional, la asistencia obligatoria. </b></p>
+                <p><b>El regalo es opcional la asistencia obligatoria. </b></p>
+                <p><b>LA ASISTENCIA OBLIGATORIA. </b></p>
                 <p>Pero si deseas tener un detalle con nosotros, puedes hacerlo en efectivo depositandolo en el buzón el día de la boda o
-                    también puedes entrar al siguiente link para ver nuestra lista de deseos:
+                    también puedes entrar al siguiente link para adquirir algo de nuestra lista de deseos:
                 </p><br />
                 <a target="_blank" href="https://www.cemaco.com/list/aculopezluxchajon">https://www.cemaco.com/list/aculopezluxchajon</a>
             </>
